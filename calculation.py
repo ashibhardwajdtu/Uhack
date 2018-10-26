7ffb1b986e1c872094159fc1580ca37695a51dff
ay2306 <mahajan.ayush2306@gmail.com>
2018-10-27 00:51:28 +0530

    new try and except

--- calculation.py
+++ calculation.py
@@ -20,7 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.0, rando
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+# X_test = sc.transform(X_test)
 
 import keras
 from keras.models import Sequential
